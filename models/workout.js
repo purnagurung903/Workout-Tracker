eebad2096b4299fa88b00d383c955443e1809daf
Purna Gurung <purnagurung903@gmail.com>
2021-03-15 17:49:35 -0400

    forgot to commit earlier added routes added models

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,53 @@
 const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+
+
+  day: {
+    type: Date,
+    default: Date.now
+
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter a type of workout"
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for workout"
+
+      },
+      duration: {
+        type: Number,
+        required: "Enter a valid duration"
+      },
+      weight: {
+        type: Number,
+        default: 0,
+        required: "Enter a weight"
+      },
+      reps: {
+        type: Number,
+        default: 0,
+        required: "Enter a total number of reps"
+      },
+      sets: {
+        type: Number,
+        default: 0,
+        required: "Enter a total number of weights"
+      }
+    }
+  ]
+
+
+});
+
+const Workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
