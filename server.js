d61d3aa886664cf827a52c7fadbae2ce3bc9b9e8
Purna Gurung <purnagurung903@gmail.com>
2021-03-15 20:28:31 -0400

    deploying

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,useFindAndModify: false
 });
 
 // routes
